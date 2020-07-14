d0cbb31409cd261e7b58d063b688acd818bfe6e2
akhilgoel0007 <akhilgoeljan@gmail.com>
2020-07-14 15:29:43 +0530

    Progress in Resume Generator

--- Newproject/views.py
+++ Newproject/views.py
@@ -96,11 +96,11 @@ def GeneratedResume(request):
 	Skills = DisplayConfigure('Skills', RefineList(request.GET.getlist('Skill', 'default'))) 
 
 	# Candidate Interest Set
-	Interest = DisplayConfigure('Interests', RefineList(request.GET.getlist('Interest', 'default'))) 
+	Interest = DisplayConfigure('Interest', RefineList(request.GET.getlist('Interest', 'default'))) 
 
 	# Get All The Education Fields..
 	Education = DisplayConfigure('Education', EducationData(RefineList(request.GET.getlist('Field', 'default')), RefineList(request.GET.getlist('College', 'default')), RefineList(request.GET.getlist('StudyStartTime', 'default')), RefineList(request.GET.getlist('StudyEndTime', 'default')), RefineList(request.GET.getlist('GPA', 'default'))))
-	
+
 	# Get All The Personal Projects..
 	PersonalProjects = DisplayConfigure('PersonalProjects', ProjectData(RefineList(request.GET.getlist('ProjectHeading', 'default')), RefineList(request.GET.getlist('ProjectStartTime', 'default')), RefineList(request.GET.getlist('ProjectEndTime', 'default')), RefineList(request.GET.getlist('ProjectDescription', 'default'))))
 	
@@ -119,33 +119,120 @@ def GeneratedResume(request):
 		'Location': Location,
 		'LinkedIn': LinkedIn,
 		'Github': Github,
-		'Skills': Skills,
-		'Interests': Interest,
 		'Education': Education,
 		'PersonalProjects': PersonalProjects,
+		'Skills': Skills,
 		'Achievements': Achievements,
-		'Certifications': Certifictions
+		'Certifications': Certifictions,
+		'Interests': Interest
 	}
 
-	# Name = 'Akhil Goel'
-	# Designation = 'Electrical Engineering Undergraduate'
-	# Description = '''I am a 2nd year Electrical Engineering student with a good command over programming languages. A programming Enthusiast with a sheer will to learn and grow in the field of programming'''
-	# Gmail = {'Display': True, 'Value': 'akhilgoeljan@gmail.com'}
-	# PhoneNumber = {'Display': True, 'Value': '0941757493'} 
-	# Location = {'Display': True, 'Value': 'Chandigarh, India'} 
-	# LinkedIn = {'Display': True, 'Value': 'linkedin.com/in/akhil-goel-a5a666176'} 
-	# Github = {'Display': True, 'Value': 'github.com/akhilgoel0007'}
-	
-	# NewParameters = {
-	# 	'Name': Name,
-	# 	'Designation': Designation,
-	# 	'Description': Description,
-	# 	'Gmail': Gmail,
-	# 	'PhoneNumber': PhoneNumber,
-	# 	'Location': Location,
-	# 	'LinkedIn': LinkedIn,
-	# 	'Github': Github,
-	# }
+	Name = 'Akhil Goel'
+	Designation = {'Display': True, 'Designation': 'Electrical Engineering Undergraduate'}
+	Description = {'Display': True, 'Description': 'I am a 2nd year Electrical Engineering student with a good command over programming languages. A programming Enthusiast with a sheer will to learn and grow in the field of programming'}
+	Gmail = {'Display': True, 'Gmail': 'akhilgoeljan@gmail.com'}
+	PhoneNumber = {'Display': True, 'PhoneNumber': '0941757493'} 
+	Location = {'Display': True, 'Location': 'Chandigarh, India'} 
+	LinkedIn = {'Display': True, 'LinkedIn': 'linkedin.com/in/akhil-goel-a5a666176'} 
+	Github = {'Display': True, 'Github': 'github.com/akhilgoel0007'}
+	Education = [
+		{
+			'Field': 'Bachelor of Technology (B.Tech)',
+			'College': 'Punjab Engineering College, Chandigarh',
+			'StartTime': '08/2018',
+			'EndTime': 'Present',
+			'GPA': '8.46/10.0'
+		},
+		{
+			'Field': 'Class XII',
+			'College': 'D.C. Montessori Smart School, Chandigarh',
+			'StartTime': '03/2016',
+			'EndTime': '03/2018',
+			'GPA': '92%'
+		},
+	]
 
-	# return render(request, 'GeneratedResume.html', NewParameters)
-	return render(request, 'GeneratedResume.html', Parameters)
+	PersonalProjects = [
+		{
+			'Heading': 'Chess Game Simulator',
+			'StartTime': '06/2019',
+			'EndTime': '09/2019',
+			'Description': 'The Application is made using python only (GUI is made using tkinter library). Application can be used to visulaize any chess game which is stored in form of sentences. (Source code on my github account)'
+		},
+		{
+			'Heading': 'HeadBeats - Desktop Music Application',
+			'StartTime': '01/2020',
+			'EndTime': '04/2020',
+			'Description': 'It is a desktop music application with a frontend made in vue.js, electron.js, HTML, HTML Canvas, CSS and javascript. The Data is Stored in a JSON file which is edited and read using fs module of Node.js. (Source code on my github account)'
+		}
+	]
+
+	Skills = ['C++', 'Ruby', 'Python', 'Data Structures','Algorithms', 'HTML5', 'CSS', 'Javascript', 'Node.js', 'Vue.js', 'Electron.js', 'Web scrapping', 'Git']
+
+	Achievements = [
+		{
+			'Name': 'JEE Mains 2018', 
+			'Date': '06/2018', 
+			'Description': 'Secured a rank of 32,949 in JEE Mains 2018' 
+		},
+		{
+			'Name': 'Talent Hunt by Career Launcher', 
+			'Date': '10/2018', 
+			'Description': 'Secured 2 position in a 3 stage competition organised by career launcher company' 
+		}
+	]
+
+	Certifications = [
+		{
+			'Heading': 'Algorithmic Toolbox course',
+			'StartTime': ' 12/2019',
+			'EndTime': '01/2020',
+			'Description': 'Course on Mathematical aspect of algorithms and their implementation'
+		},
+		{
+			'Heading': 'Data Structures course',
+			'StartTime': '02/2020',
+			'EndTime': '03/2020',
+			'Description': 'Course on basic data structures and their implementation'
+		},
+		{
+			'Heading': 'Algorithms on Graphs',
+			'StartTime': '03/2020',
+			'EndTime': '04/2020',
+			'Description': 'Course on graph algorithms and their implementation'
+		},
+		{
+			'Heading': 'Algorithms on Strings',
+			'StartTime': '03/2020',
+			'EndTime': '04/2020',
+			'Description': 'Course on String algorithms and their implementation'
+		}
+	]
+
+	Interest = ['Chess', 'Travelling', 'Competitive', 'Programming', 'Automating Tasks']
+	PersonalProjects = {'Display': True, 'PersonalProjects': PersonalProjects}
+	Skills = {'Display': True, 'Skills': Skills}
+	Achievements = {'Display': True, 'Achievements': Achievements}
+	Education = {'Display': True, 'Education': Education}
+	Interest = {'Display': True, 'Interest': Interest}
+	Certifications = {'Display': True, 'Certifications': Certifications}
+
+	NewParameters = {
+		'Name': Name,
+		'Designation': Designation,
+		'Description': Description,
+		'Gmail': Gmail,
+		'PhoneNumber': PhoneNumber,
+		'Location': Location,
+		'LinkedIn': LinkedIn,
+		'Github': Github,
+		'Education': Education,
+		'PersonalProjects': PersonalProjects,
+		'Skills': Skills,
+		'Achievements': Achievements,
+		'Certifications': Certifictions,
+		'Interest': Interest
+	}
+
+	return render(request, 'GeneratedResume.html', NewParameters)
+	# return render(request, 'GeneratedResume.html', Parameters)
