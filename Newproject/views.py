2c0a50df316b7802b4c9c1f219978fcb14dd9a61
akhilgoel0007 <akhilgoeljan@gmail.com>
2020-07-20 13:36:06 +0530

    New Modifications

--- Newproject/views.py
+++ Newproject/views.py
@@ -9,6 +9,7 @@ def DisplayConfigure(ParameterName, Parameter):
 
 	if not len(Parameter):
 		Display['Display'] = False
+	print(f"{ParameterName}: {Display}")
 	
 	return Display 
 
@@ -76,10 +77,15 @@ def CertificateData(Heading, StartTime, EndTime, Description):
 	return AllCertificates
 
 def HomePage(request):
-	return render(request, 'Home.html', Parameters)
+	return render(request, 'Home.html')
 
 def FormPage(request):
-	return render(request, 'ResumeForm.html')
+	Parameters = {
+		'EducationFieldCount': 1
+	}
+	# Parameters = 1 
+
+	return render(request, 'ResumeForm.html', Parameters)
 
 def GeneratedResume(request):
 	Name = request.POST.get('Name') # Candidate Name
@@ -123,114 +129,7 @@ def GeneratedResume(request):
 		'Skills': Skills,
 		'Achievements': Achievements,
 		'Certifications': Certifications,
-		'Interests': Interest
+		'Interest': Interest
 	}
-	
-	# Name = 'Akhil Goel'
-	# Designation = {'Display': True, 'Designation': 'Electrical Engineering Undergraduate'}
-	# Description = {'Display': True, 'Description': 'I am a 2nd year Electrical Engineering student with a good command over programming languages. A programming Enthusiast with a sheer will to learn and grow in the field of programming'}
-	# Gmail = {'Display': True, 'Gmail': 'akhilgoeljan@gmail.com'}
-	# PhoneNumber = {'Display': True, 'PhoneNumber': '0941757493'} 
-	# Location = {'Display': True, 'Location': 'Chandigarh, India'} 
-	# LinkedIn = {'Display': True, 'LinkedIn': 'linkedin.com/in/akhil-goel-a5a666176'} 
-	# Github = {'Display': True, 'Github': 'github.com/akhilgoel0007'}
-	# Education = [
-	# 	{
-	# 		'Field': 'Bachelor of Technology (B.Tech)',
-	# 		'College': 'Punjab Engineering College, Chandigarh',
-	# 		'StartTime': '08/2018',
-	# 		'EndTime': 'Present',
-	# 		'GPA': '8.46/10.0'
-	# 	},
-	# 	{
-	# 		'Field': 'Class XII',
-	# 		'College': 'D.C. Montessori Smart School, Chandigarh',
-	# 		'StartTime': '03/2016',
-	# 		'EndTime': '03/2018',
-	# 		'GPA': '92%'
-	# 	},
-	# ]
-
-	# PersonalProjects = [
-	# 	{
-	# 		'Heading': 'Chess Game Simulator',
-	# 		'StartTime': '06/2019',
-	# 		'EndTime': '09/2019',
-	# 		'Description': 'The Application is made using python only (GUI is made using tkinter library). Application can be used to visulaize any chess game which is stored in form of sentences. (Source code on my github account)'
-	# 	},
-	# 	{
-	# 		'Heading': 'HeadBeats - Desktop Music Application',
-	# 		'StartTime': '01/2020',
-	# 		'EndTime': '04/2020',
-	# 		'Description': 'It is a desktop music application with a frontend made in vue.js, electron.js, HTML, HTML Canvas, CSS and javascript. The Data is Stored in a JSON file which is edited and read using fs module of Node.js. (Source code on my github account)'
-	# 	}
-	# ]
-
-	# Skills = ['C++', 'Ruby', 'Python', 'Data Structures','Algorithms', 'HTML5', 'CSS', 'Javascript', 'Node.js', 'Vue.js', 'Electron.js', 'Web scrapping', 'Git']
-
-	# Achievements = [
-	# 	{
-	# 		'Name': 'JEE Mains 2018', 
-	# 		'Date': '06/2018', 
-	# 		'Description': 'Secured a rank of 32,949 in JEE Mains 2018' 
-	# 	},
-	# 	{
-	# 		'Name': 'Talent Hunt by Career Launcher', 
-	# 		'Date': '10/2018', 
-	# 		'Description': 'Secured 2 position in a 3 stage competition organised by career launcher company' 
-	# 	}
-	# ]
-
-	# Certifications = [
-	# 	{
-	# 		'Heading': 'Algorithmic Toolbox course',
-	# 		'StartTime': '12/2019',
-	# 		'EndTime': '01/2020',
-	# 		'Description': 'Course on Mathematical aspect of algorithms and their implementation'
-	# 	},
-	# 	{
-	# 		'Heading': 'Data Structures course',
-	# 		'StartTime': '02/2020',
-	# 		'EndTime': '03/2020',
-	# 		'Description': 'Course on basic data structures and their implementation'
-	# 	},
-	# 	{
-	# 		'Heading': 'Algorithms on Graphs',
-	# 		'StartTime': '03/2020',
-	# 		'EndTime': '04/2020',
-	# 		'Description': 'Course on graph algorithms and their implementation'
-	# 	},
-	# 	{
-	# 		'Heading': 'Algorithms on Strings',
-	# 		'StartTime': '03/2020',
-	# 		'EndTime': '04/2020',
-	# 		'Description': 'Course on String algorithms and their implementation'
-	# 	}
-	# ]
-
-	# Interest = ['Chess', 'Travelling', 'Competitive', 'Programming', 'Automating Tasks']
-	# PersonalProjects = {'Display': True, 'PersonalProjects': PersonalProjects}
-	# Skills = {'Display': True, 'Skills': Skills}
-	# Achievements = {'Display': True, 'Achievements': Achievements}
-	# Education = {'Display': True, 'Education': Education}
-	# Interest = {'Display': True, 'Interest': Interest}
-	# Certifications = {'Display': True, 'Certifications': Certifications}
-
-	# NewParameters = {
-	# 	'Name': Name,
-	# 	'Designation': Designation,
-	# 	'Description': Description,
-	# 	'Gmail': Gmail,
-	# 	'PhoneNumber': PhoneNumber,
-	# 	'Location': Location,
-	# 	'LinkedIn': LinkedIn,
-	# 	'Github': Github,
-	# 	'Education': Education,
-	# 	'PersonalProjects': PersonalProjects,
-	# 	'Skills': Skills,
-	# 	'Achievements': Achievements,
-	# 	'Certifications': Certifications,
-	# 	'Interest': Interest
-	# }
 
 	return render(request, 'GeneratedResume.html', Parameters)
